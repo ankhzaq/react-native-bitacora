51ab35c535e936d0b593df38535ce77e280e0696
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-01-02 19:07:34 +0100

    feat: base project

---
+++ Screens/Home.tsx
@@ -0,0 +1,210 @@
+import { View, Text, FlatList, StyleSheet, Pressable, TextInput, TouchableOpacity, Keyboard } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { firebase } from '../config';
+import { FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
+
+const Home = () => {
+    const [data, setData] = useState([]);
+    const dataRef = firebase.firestore().collection('bitacora');
+    const [description, setDescription] = useState('');
+    const [tag, setTag] = useState('');
+    const [title, setTitle] = useState('');
+    const navigation = useNavigation();
+
+    // fetch or read the data from firestore
+    useEffect(() => {
+        dataRef
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(
+            querySnapshot => {
+            const initialData = []
+            querySnapshot.forEach((doc) => {
+                const item = doc.data()
+                initialData.push(item)
+            })
+            setData(initialData)
+            //console.log(users)
+        })
+    }, [])
+
+    // delete an item from firestore db
+    const deleteItem = (item) => {
+        dataRef
+            .doc(item.id)
+            .delete()
+            .then(() => {
+                alert("Deleted successfully");
+            })
+            .catch(error => {
+                alert(error);
+            })
+    }
+
+    // add a todo
+    const addItem = () => {
+        // check if we have a todo.
+        if (tag.length) {
+            // get the timestamp
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+            const data = {
+                createdAt: timestamp,
+                description,
+                email: "darthzaq@gmail.com",
+                private: false,
+                tag,
+                title,
+            };
+            if (!data.title || !data.title.length) delete data.title;
+            if (!data.description || !data.description.length) delete data.description;
+            dataRef
+                .add(data)
+                .then(() => {
+                    // release todo state
+                    setTitle('');
+                    setDescription('');
+                    // release keyboard
+                    Keyboard.dismiss();
+                })
+                .catch((error) => {
+                    // show an alert in case of error
+                    alert(error);
+                })
+        }
+    }
+
+    const getOptionsPicker = () => {
+        const optionsSelect = [];
+        data.forEach(({ tag }) => {
+            if (!optionsSelect.includes(tag)) {
+                optionsSelect.push(tag);
+            }
+        });
+        return optionsSelect;
+    }
+
+    return (
+        <View style={{flex:1}}>
+            <View style={styles.formContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder='Tag'
+                  placeholderTextColor="#aaaaaa"
+                  onChangeText={(text) => setTag(text)}
+                  value={tag}
+                  underlineColorAndroid="transparent"
+                  autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Title'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setTitle(text)}
+                    value={title}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder='Description'
+                  placeholderTextColor="#aaaaaa"
+                  onChangeText={(text) => setDescription(text)}
+                  value={description}
+                  underlineColorAndroid="transparent"
+                  autoCapitalize="none"
+                />
+            </View>
+            <TouchableOpacity style={styles.button} onPress={addItem}>
+                <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+            <FlatList
+                style={{}}
+                data={data}
+                numColumns={1}
+                renderItem={({ item }) => (
+                    <View>
+                        <Pressable
+                        style={styles.container}
+                          // @ts-ignore
+                        onPress={() => navigation.navigate('Detail', {item})}
+                        >
+                            <FontAwesome name="trash-o"
+                            color="red"
+                            // @ts-ignore
+                            onPress={() => deleteItem(item)}
+                            style={styles.todoIcon} />
+                            <View style={styles.innerContainer}>
+                                <Text style={styles.itemHeading}>
+                                    {item.tag}{item.title && ` - ${item.title}`}
+                                </Text>
+                            </View>
+
+                        </Pressable>
+                    </View>
+
+
+                )}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e5e5e5',
+        padding: 15,
+        borderRadius: 15,
+        margin:5,
+        marginHorizontal: 10,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    innerContainer: {
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginLeft:45,
+    },
+    itemHeading: {
+        fontWeight: 'bold',
+        fontSize:18,
+        marginRight:22
+    },
+    formContainer: {
+        flexDirection: 'row',
+        height: 80,
+        marginLeft:10,
+        marginRight: 10,
+        marginTop:100
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        paddingLeft: 16,
+        flex: 1,
+        marginRight: 5
+    },
+    button: {
+        height: 47,
+        borderRadius: 5,
+        backgroundColor: '#788eec',
+        marginBottom: 15,
+        width: '80%',
+        alignSelf: 'center',
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 20
+    },
+
+    todoIcon:{
+        marginTop:5,
+        fontSize:20,
+        marginLeft:14,
+    },
+});
+
+export default Home
