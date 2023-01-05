2edb1b915fb189a7059cfb083c32462b93705e32
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-01-05 15:00:00 +0100

    fix: home styles

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -184,26 +184,27 @@ const styles = StyleSheet.create({
         marginRight:22
     },
     formContainer: {
-        flexDirection: 'row',
-        height: 80,
+        flexDirection: 'column',
+        height: 150,
         marginLeft:10,
         marginRight: 10,
         marginTop:100
     },
     input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
         backgroundColor: 'white',
-        paddingLeft: 16,
+        borderRadius: 5,
         flex: 1,
-        marginRight: 5
+        height: 52,
+        marginHorizontal: 5,
+        marginVertical: 5,
+        overflow: 'hidden',
+        paddingLeft: 16,
     },
     button: {
         height: 47,
         borderRadius: 5,
         backgroundColor: '#788eec',
-        marginBottom: 15,
+        marginVertical: 15,
         width: '80%',
         alignSelf: 'center',
         alignItems: "center",
